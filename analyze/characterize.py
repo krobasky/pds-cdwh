a2130191f3a6fdd8b2d2af7d253a4fe60ae2c7cd
Kimberly Robasky <krobasky@gmail.com>
2019-11-16 01:45:42 -0500

    code is now funcitonal

--- analyze/characterize.py
+++ analyze/characterize.py
@@ -8,69 +8,70 @@ patvarFilename="../out/DOAC/all/bleeding.patvars"
 df = pd.read_csv(patvarFilename,
                  sep="\t", 
                  names=["pid", "age", "yearsDead", "sex", "race", "ethnic", "married", "religion", "zip", "zippath", "AEFlag"]);
-
 print(df.head());
-print(df[df["AEFlag"]==1,"AEFlag"].describe());
-print(df[df["AEFlag"]==2].describe());
-controls=df[df["AEFlag"]==2].sample(n=4338, random_state=1)
-cases=df[df["AEFlag"]==1]
+aestats=df[df["AEFlag"]==2].describe();
+total_aes=aestats.loc["count","pid"]
+print("Total adverse events [4338] = " + str(total_aes) );
 
-df[df["AEFlag"]==1].describe();
-df[df["AEFlag"]==2].describe();
-controls=df[df["AEFlag"]==2].sample(n=4338, random_state=1)
-cases=df[df["AEFlag"]==1]
-cases.describe()
-controls.describe()
-controls.head()
-controls[controls["race"]=="1"].describe()
-controls[controls["race"]!="1"].describe()
-cases[cases["race"]=="1"].describe()
-cases[cases["race"]!="1"].describe()
-cases[cases["sex"]=="F"].describe()
-cases[cases["sex"]=="M"].describe()
-controls[controls["sex"]=="F"].describe()
-controls[controls["sex"]=="M"].describe()
-controls["religion"].describe()
-#controls["religion"].types
+cases=df[df["AEFlag"]==2]
+print("*********CASES",end="");
+numCases=cases.describe().loc['count','age']
+print("["+str(numCases)+"]");
+print(cases.describe().loc[['mean','min','max'],'age':'yearsDead']);
+
+controls=df[df["AEFlag"]==1].sample(n=int(total_aes), random_state=1)
+print("*****CONTROLS",end="");
+numControls=controls.describe().loc['count','age']
+print("["+str(numControls)+"]");
+print(controls.describe().loc[['mean','min','max'],'age':'yearsDead']);
+
+
+print("Female: cases, controls");
+totalFemaleCases=cases[cases["sex"]=="F"].describe().loc['count','age'];
+totalFemaleControls=controls[controls["sex"]=="F"].describe().loc['count','age'];
+print("" + str(int(100*totalFemaleCases/cases.describe().loc['count','age']))+"%",end="");
+print(", " + str(int(100*totalFemaleControls/controls.describe().loc['count','age']))+"%");
+
+print("Caucasion: cases, controls");
+totalCauCases=cases[cases["race"]=="1"].describe().loc['count','age'];
+totalCauControls=controls[controls["race"]=="1"].describe().loc['count','age'];
+print("" + str(int(100*totalCauCases/cases.describe().loc['count','age']))+"%", end="");
+print(", " + str(int(100*totalCauControls/controls.describe().loc['count','age']))+"%");
+
+relstr="NOT"
+totalNonRelCases=cases[cases["religion"]==relstr].describe().loc['count','age'];
+totalNonRelControls=controls[controls["religion"]==relstr].describe().loc['count','age'];
+print("Religion - "+relstr+"["+str(totalNonRelCases)+","+str(totalNonRelControls)+"] : cases, controls");
+#print("Religion - "+relstr+": cases, controls");
+print("" + str(int(100*totalNonRelCases/cases.describe().loc['count','age']))+"%", end="");
+print(", " + str(int(100*totalNonRelControls/controls.describe().loc['count','age']))+"%");
+
+relstr="UNKNOWN"
+totalNonRelCases=cases[cases["religion"]==relstr].describe().loc['count','age'];
+totalNonRelControls=controls[controls["religion"]==relstr].describe().loc['count','age'];
+print("Religion - "+relstr+"["+str(totalNonRelCases)+","+str(totalNonRelControls)+"] : cases, controls");
+print("" + str(int(100*totalNonRelCases/cases.describe().loc['count','age']))+"%", end="");
+print(", " + str(int(100*totalNonRelControls/controls.describe().loc['count','age']))+"%");
+
+cases.groupby(["religion"]).size()
+x=cases.groupby(["religion"]).size()
+#print(x.sort_values())
+#print(x.sort_values().tail())
+
 controls.groupby(["religion"]).size()
 x=controls.groupby(["religion"]).size()
-x.head()
-x.sort_values()
-y=cases.groupby(["religion"]).size()
-controls["religion"].describe()
-controls.head()
-controls["religion"].describe()
-x.sort_values().tail()
-y.sort_values().tail()
-controls.groupby(["sex"]).size()
-cases.groupby(["sex"]).size()
-cases.groupby(["race"]).size()
-controls.groupby(["race"]).size()
-cases.groupby(["race"]).size()
-controls.groupby(["religion"]).size()
-y.sort_values().tail()
-x.sort_values().tail()
-contrAge=controls.groupby(["age"]).size()
-caseAge=cases.groupby(["age"]).size()
-contrAge.sort_values().tail()
-caseAge.sort_values().tail()
-contrDeath=controls.groupby(["age"]).size()
-cases.head()
-caseDeath=cases.groupby(["yearsDead"]).size()
-contDeath=controls.groupby(["yearsDead"]).size()
-caseDeath.sort_values().tail()
-contDeath.sort_values().tail()
-contDeath.describe()
-caseDeath.describe()
-cases["yearsDead"].describe()
-controls["yearsDead"].describe()
-sum(controls["yearsDead"])
-sum(cases["yearsDead"])
-cases[cases["yearsDead"]>0].describe()
-controls[controls["yearsDead"]>0].describe()
-controls["age"].describe()
-cases["age"].describe()
-cases.corr()
 
+# -------
 
+df = pd.read_csv("tmp.csv", sep='\t', 
+                 names=["species","count", "mean", "std","min", "twentyfive", "seventyfive",  "max"]);
+print(df.head());
 
+#df.groupby("species")['petal_length'].describe();
+#setosa = df[(df['species'] == 'Iris-setosa')]
+#virginica = df[(df['species'] == 'Iris-virginica')]
+#
+#from scipy import stats;
+#stats.shapiro(setosa['petal_length'])
+#
+#print(stats.shapiro(setosa['petal_length']))
